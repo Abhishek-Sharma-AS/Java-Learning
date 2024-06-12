66c0f8fd53fd6f41c37084871dd51180c5947665
Abhishek Sharma <abhello12@gamil.com>
2024-06-12 22:59:12 +0530

    String

---
+++ String in Java/MyClass.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * MyClass
+ */
+public class MyClass {
+  public static void main(String args[])
+  {
+     
+      String s = new String("Abhishek Sharma");
+      
+    
+          System.out.println(s.toString());
+ 
+     
+  }
+}
